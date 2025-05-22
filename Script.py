a305944a7301f646c621b5550c8c0ed327077c0f
mohn_bgs <ssmemes163@gmail.com>
2025-05-22 10:31:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -311,7 +311,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/+YgB1verq735mNzll">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/Thebhumihar>[𝘽𝙃𝙐𝙈𝙄𝙃𝘼𝙍]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/+YgB1verq735mNzll">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/Thebhumihar>[𝘽𝙃𝙐𝙈𝙄𝙃𝘼𝙍]</a></b>\n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n⚡𝐉𝐎𝐈𝐍:-BACKUP{https://t.me/+t4FtVzBmcRhjNzI1}\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"""
     
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
